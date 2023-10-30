44f6de1414c793d00b1200223b1d2fb6e9493ffa
Kakeru1011 <2101167@s.asojuku.ac.jp>
2023-10-30 11:26:49 +0900

    データベース接続を修正

--- 04_ソースコード/DBManager/DBConnect.php
+++ 04_ソースコード/DBManager/DBConnect.php
@@ -1,8 +1,8 @@
 <?php
     class DBConnect{
         public function dbConnect(){
-            $pdo = new PDO('mysql:host=mysql214.phy.lolipop.lan;dbname=LAA1417860-grp1;charset=utf8',
-							'LAA1417860', 'grp1kaihatu');
+            $pdo = new PDO('mysql:host=mysql219.phy.lolipop.lan;dbname=LAA1417860-grp1db;charset=utf8',
+							'LAA1417860', 'grp1db');
 		    return $pdo;
         }
     }
