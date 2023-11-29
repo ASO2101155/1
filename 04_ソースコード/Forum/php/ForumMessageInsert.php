d21c2db2ed0d9910ea2738909f1c1af8a2a9662e
志水 <2101166@s.asojuku.ac.jp>
2023-11-29 23:24:10 +0900

    通知(掲示板)

--- 04_ソースコード/Forum/php/ForumMessageInsert.php
+++ 04_ソースコード/Forum/php/ForumMessageInsert.php
@@ -1,6 +1,13 @@
 <?php 
      require_once '../../DBManager/DBAccess.php';
+     require_once '../../Notice/php/ForumNoticeSend.php';
      $cls = new DBAccess();
+     $forumnoticesendcls = new ForumNoticeSend();
+     if(!isset($_SESSION)){
+         session_start();
+     }
+     $user_id = $_SESSION['user_id'];
+
      try {
          //ForumPost.htmlからpostで受け取ったデータを引数にしてInsert処理
          $cls->InsertForumMessageTbl($_POST['forum_id'],$_POST['user_id'],$_POST['message_content']);
@@ -9,6 +16,11 @@
          header('Location: ../ForumDetail.htmlforum_id='.$_POST['forum_id']);
          exit;
      }
+
+     //通知送信
+     $searchArray = $cls->SelectForumMessageByForumId($_POST['forum_id']);
+     $forumnoticesendcls->forumNoticeSend($_POST['message_content'], $searchArray[0]['user_id'], $user_id, $_POST['forum_id']);
+
      //掲示板詳細画面に遷移
      header('Location: ../ForumDetail.html?forum_id='.$_POST['forum_id']);
      exit;
