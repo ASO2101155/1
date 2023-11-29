e00d559b7dfcd5169e8b5c95758c2176036618c3
志水 <2101166@s.asojuku.ac.jp>
2023-11-30 00:09:06 +0900

    Forum GETからPOSTに

--- 04_ソースコード/Forum/php/ForumMessageInsert.php
+++ 04_ソースコード/Forum/php/ForumMessageInsert.php
@@ -13,15 +13,15 @@
          $cls->InsertForumMessageTbl($_POST['forum_id'],$_POST['user_id'],$_POST['message_content']);
      } catch (Exception $ex) {
          //throw $th;
-         header('Location: ../ForumDetail.htmlforum_id='.$_POST['forum_id']);
+         header('Location: ../ForumDetail.html?insert',true, 307);
          exit;
      }
 
      //通知送信
-     $searchArray = $cls->SelectForumMessageByForumId($_POST['forum_id']);
-     $forumnoticesendcls->forumNoticeSend($_POST['message_content'], $searchArray[0]['user_id'], $user_id, $_POST['forum_id']);
+    //  $searchArray = $cls->SelectForumMessageByForumId($_POST['forum_id']);
+    //  $forumnoticesendcls->forumNoticeSend($_POST['message_content'], $searchArray[0]['user_id'], $user_id, $_POST['forum_id']);
 
      //掲示板詳細画面に遷移
-     header('Location: ../ForumDetail.html?forum_id='.$_POST['forum_id']);
+     header('Location: ../ForumDetail.html?insert',true, 307);
      exit;
 ?>
